28e16d6b5ad84a1ecbf314343ec64924a8dd2c2c
Eugene <56676493+youngblader@users.noreply.github.com>
2024-02-22 14:45:44 +0300

    feat: init noteX app

---
+++ NoteX/NoteX/Controllers/NotesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NotesViewController.swift
+//  NoteX
+//
+//  Created by Евгений Зорич on 22.02.24.
+//
+
+import UIKit
+
+final class NotesViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+}
+
+extension NotesViewController {
+    
+}
---
+++ NoteX/NoteX/Extensions/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  NoteX
+//
+//  Created by Евгений Зорич on 22.02.24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0.02, green: 0.38, blue: 0.99, alpha: 1.00)
+    static let border = UIColor(red: 0.83, green: 0.83, blue: 0.83, alpha: 1.00)
+    static let lightBorder = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.00)
+}
